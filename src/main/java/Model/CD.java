8bc0f139d6432951cef37599bf957419d7321044
Isala Piyarisi <code@isala.me>
2019-07-27 17:32:48 +0530

    Added Song Class and Update CD in Class Digram

---
+++ src/main/java/Model/CD.java
@@ -0,0 +1,29 @@
+package Model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CD extends MusicItem {
+    private List<Song> songs;
+    private int totalDuration;
+
+    public CD(int itemID, String title, String genre, Date releaseDate, String artist, BigDecimal price) {
+        super(itemID, title, genre, releaseDate, artist, price);
+    }
+
+    public void addSong(Song song) {
+        this.songs.add(song);
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public int getTotalDuration() {
+        return totalDuration;
+    }
+
+    private void updateDotalDuration(Song song){
+        this.totalDuration += song.getDuration();
+    }
+}
